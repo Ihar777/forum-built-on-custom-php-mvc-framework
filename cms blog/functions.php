8b22f0f8bebb67d7fbc4309b24a4b1af1662b196
Ihar Petrushenka <petrushen@yahoo.com>
2018-09-07 22:12:43 +0300

    ghj

--- cms blog/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function escape($string) {
-
-return mysqli_real_escape_string($connection, trim(strip_tags($string)));
-
-}
-
-?>
---
+++ myshop/admin_area/includes/db.php
@@ -0,0 +1,8 @@
+<?php 
+$con = mysqli_connect("localhost", "Ihar777", "Ihar_2706", "ecommerce");
+
+if(mysqli_connect_errno()){
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+?>
